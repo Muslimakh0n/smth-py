00b38f474d905f05b9f78edd93c416c58e854013
Muslimakhon Shokirova <shokirovamuslimakhon@gmail.com>
2024-01-25 08:48:23 +0500

    smth lasr commit of the gamr

--- my.py
+++ my.py
@@ -49,3 +49,41 @@ def display_score(correct_guesses, guesses):
 
     score = (correct_guesses / len(questions)) * 100
     print("Your score is : " + str(score) + "%")
+
+
+def play_again():
+    response = input("Do you want to  play again? (yes/no): ")
+    response = response.upper()
+
+    if response == "YES":
+        return True
+    else:
+        return False
+
+
+questions = {
+    "Iymonning nechta farzi bor? ": "A",
+    "Kema yasagan payg'ambar kim? ": "C",
+    "Payg'ambarimizning nechta farzandlari bo'lgan? ": "B",
+    "Hozirgi hijriy sana? ": "D",
+}
+
+options = [
+    ["A. 7", "B. 4", "C. 3", "D. 9"],
+    ["A. Lut a.s.", "B. Muso a.s.", "C. Nuh a.s.", "D. Yunus a.s."],
+    ["A. 4", "B. 7 ", "C. 3", "D. 9"],
+    [
+        "A. 1442  2-ramazon",
+        "B. 1441  14-shavvol",
+        "C. 1352 23-rajab",
+        "D. 1444 4-sha'bon",
+    ],
+]
+
+new_game()
+
+
+while play_again():
+    new_game()
+
+print("Bye! Don't forgot to smile ) cuz your smile is beautiful!")
