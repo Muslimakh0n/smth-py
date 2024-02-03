7752fa213d70b99510b71be90e4e59d0d4c13d51
Muslimakhon Shokirova <shokirovamuslimakhon@gmail.com>
2024-02-03 19:55:20 +0500

    smth

--- my.py
+++ my.py
@@ -178,3 +178,14 @@ if login.lower() == "admin":
     print("Xush kelibsiz Admin, foydalanuvchilar ro'yxatini ko'rasizmi?")
 else:
     print(f"Xush kelibsiz {login.title()}!")
+
+
+ x = float(input("Birinchi sonni kiriting: "))
+y = float(input("Ikkinchi sonni kiriting:"))
+if x==y: print(f"Sonlar teng: {x}={y}")
+
+son = float(input("Istalgan son kiriting:"))
+print("Son manfiy") if son<0 else print("Son musbat")
+ 
+son = float(input('Istalgan son kiriting: '))
+print(son**(1/2)) if son>0 else print('Musbat son kiriting')
