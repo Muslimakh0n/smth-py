c7fdeef9de8b17e6ee1d15234d7d65d390689019
Muslimakhon Shokirova <shokirovamuslimakhon@gmail.com>
2024-02-08 13:41:33 +0500

    bozorlik

--- my.py
+++ my.py
@@ -212,3 +212,25 @@ if x < 0:
     print(f"This number is posotive")
 elif x > 0:
     print(f"This number is negative")  # there is something frongggggg(sabrrrrr)
+
+
+mahsulotlar = [
+    "un",
+    "yog'",
+    "guruch",
+    "tuxum",
+    "piyoz",
+    "kartoshka",
+    "olma",
+    "banan",
+    "uzum",
+    "qulupnay",
+]
+savat = []
+for n in range(5):
+    savat.append(input(f"Savatga {n+1}-mahsulotni qo'shing: "))
+for mahsulot in savat:
+    if mahsulot in mahsulotlar:
+        print(f"Do'konimizda {mahsulot} bor")
+    else:
+        print(f"Do'konimizda {mahsulot} yo'q")
