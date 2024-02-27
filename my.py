3313a56176c2cf17560e0fd2d6f708368e93f42a
Muslimakhon Shokirova <shokirovamuslimakhon@gmail.com>
2024-02-27 23:42:14 +0500

    task 2

--- my.py
+++ my.py
@@ -361,3 +361,30 @@ for ism, kinolar in kinolar.items():
     print(f"\n{ism.title()}ning sevimli kinolari:")
     for kino in kinolar:
         print(kino)
+
+        davlatlar = {
+            "o'zbekiston": {
+                "poytaxt": "toshkent",
+                "maydon": 448978,
+                "aholi": 33_000_000,
+                "pul birligi": "so'm",
+            },
+            "rossiya": {
+                "poytaxt": "moskva",
+                "maydon": 17_098_246,
+                "aholi": 144_000_000,
+                "pul birligi": "rubl",
+            },
+            "aqsh": {
+                "poytaxt": "vashington",
+                "maydon": 9_631_418,
+                "aholi": 327_000_000,
+                "pul birligi": "dollar",
+            },
+            "malayziya": {
+                "poytaxt": "kuala-lumpur",
+                "maydon": 329750,
+                "aholi": 25_000_000,
+                "pul birligi": "rinngit",
+            },
+        }
